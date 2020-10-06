05b119adddfb38ee5c5343a50958d01fa68a1192
ImalshaRathnaweera <surangirathnaweera1996@gmail.com>
2020-10-07 00:32:19 +0530

    signup changes

--- src/Component/SignUp/SignUp.js
+++ src/Component/SignUp/SignUp.js
@@ -1,215 +1,275 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Button from '@material-ui/core/Button';
+import Link from '@material-ui/core/Link';
 import G3 from "../images/G3.png";
+import axios from 'axios';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: '#aa00ff',
-    // alignItems: 'center',
-    justifyContent: 'center',
-    width: '98.93vw',
-    height: '110vh',
-    spacing: 0,
-    boxShadow: 'none',
-    textTransform: 'none',
-    fontSize: 16,
-    border: '1px solid',
-    lineHeight: 1.5,
-    borderColor: '#0063cc',
-    paddingTop: '5%'
 
-  },
-  paper: {
+class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
 
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-  welcome: {
-    fontFamily: 'Roboto',
-    textAlign: 'center',
-    fontSize: '1.8rem'
 
-  },
-  ebus: {
-    fontFamily: 'Roboto',
-    textAlign: 'center',
-    color: 'white',
-    fontSize: '2.5rem'
-  },
+    this.state = {
+      name: "",
+      email: "",
+      address: "",
+      nic: "",
+      contact: "",
+      password: "",
+      confirmpassword: "",
+    }
+    this.namehandler = this.namehandler.bind(this);
+    this.emailhandler = this.emailhandler.bind(this);
+    this.addresshandler = this.addresshandler.bind(this);
+    this.nichandler = this.nichandler.bind(this);
+    this.contacthandler = this.contacthandler.bind(this);
+    this.passwordhandler = this.passwordhandler.bind(this);
+    this.confirmpasswordhandler = this.confirmpasswordhandler.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  namehandler = (event) => {
+    this.setState({
+      name: event.target.value
+    })
+    console.log(event.target.value)
+  }
+  emailhandler = (event) => {
+    this.setState({
+      email: event.target.value
+    })
+    console.log(event.target.value)
+  }
+  addresshandler = (event) => {
+    this.setState({
+      address: event.target.value
+    })
+    console.log(event.target.value)
+  }
+  nichandler = (event) => {
+    this.setState({
+      nic: event.target.value
+    })
+    console.log(event.target.value)
+  }
+  contacthandler = (event) => {
+    this.setState({
+      contact: event.target.value
+    })
+    console.log(event.target.value)
+  }
+  passwordhandler = (event) => {
+    this.setState({
+      password: event.target.value
+    })
+    console.log(event.target.value)
+  }
+  confirmpasswordhandler = (event) => {
+    this.setState({
+      confirmpassword: event.target.value
+    })
+    console.log(event.target.value)
+  }
 
-  button: {
-    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-    borderRadius: 8,
-    border: 0,
-    color: 'white',
-    height: 48,
-    width: "30%",
-    padding: '10px 30px',
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    justifyContent: 'center'
-  },
-  image: {
-    height: '100px',
-    width: '150px',
-    paddingTop: '10%'
+  handleSubmit() {
+    const data = {
+      name : this.state.name,
+      email : this.state.email,
+      address: this.state.address,
+      nic: this.state.nic,
+      contact: this.state.contact,
+      password : this.state.password
+    }
+    // alert(`${this.state.email} ${this.state.password} Success`)
+    axios.post('http://localhost:3000/api/user/registerWeb', data)
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => {
+      console.log(err.message);
+      console.log(err.status);
+    })
+      this.props.history.push('/dashboard');
+    // event.preventDefault()
+  }
 
-  },
-  card: {
-    maxWidth: '180px',
-    backgroundColor: 'transparent',
-    boxShadow: 'none',
-  },
-  avatar: {
-    backgroundColor: 'black',
-  },
-}));
+  render() {
+    return (
 
-export default function SignIn() {
-  const classes = useStyles();
+      <Grid container style={{
+        flexGrow: 1,
+        backgroundColor: '#a503fc',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100vw',
+        height: '150vh',
+        spacing: 0,
 
-  return (
-    <Grid container className={classes.root}>
-      <Grid item xs={12} sm={6} >
-        <Typography component="h1" variant="" className={classes.ebus}>
-          eBus
-        </Typography>
-        {/* <Card className={classes.card}>
-                <CardMedia
-                  component="img"
-                  alt="eBus image"
-                  height="150px"
-                  image={G3}
-                  title="eBus image"
-                />
-        </Card> */}
-
-        <Paper style={
-          {
-            padding: '20px 20px',
-            // margin: 50,
+      }}>
+        <Grid item xs={12} sm={6} >
+          <Typography component="h1" variant="" style={{
             textAlign: 'center',
+            color: 'white',
+            fontSize: '2.5rem',
+          }}>
+            eBus
+            </Typography>
+          <Card style={{
+            backgroundColor: 'transparent',
+            boxShadow: 'none',
+            marginTop: 20,
+            height: '150px',
+            width: '200px',
+            position: 'fixed',
+
+
+          }}>
+          </Card>
+
+          <Paper style={{
+            padding: '20px 20px',
+            margin: 50,
+            textAlign: 'center',
+            borderRadius: 15,
+
           }}>
 
+            <form >
+              <Typography variant={'h5'}>
+                <h3> Sign In </h3>
+              </Typography>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="username"
+                label="User Name"
+                name="username"
+                autoComplete="User Name"
+                autoFocus
+                value={this.state.name}
+                onChange={this.namehandler}
+              />
 
-          <form>
-            {/* <Card className={classes.card}>
-              
-            <CardMedia
-             component="img"
-             alt="eBus image"
-             height="150"
-             image={G3}
-             title="eBus image"
-            />
-            </Card> */}
+              <TextField
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                id="email"
+                label="Email"
+                name="email"
+                autoComplete="email"
+                value={this.state.email}
+                onChange={this.emailhandler}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="address"
+                label="Address"
+                name="address"
+                autoComplete="address"
+                value={this.state.address}
+                onChange={this.addresshandler}
+              />
 
-            <Typography component="h2" variant="" className={classes.welcome}>
-              User Registration
-            </Typography>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="nic"
+                label="NIC Number"
+                name="nic"
+                autoComplete="NIC"
+                value={this.state.nic}
+                onChange={this.nichandler}
+              />
 
-
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="username"
-              label="User Name"
-              name="username"
-              autoComplete="User Name"
-            />
-
-            <TextField
-              variant="outlined"
-              margin="normal"
-              fullWidth
-              id="email"
-              label="Email"
-              name="email"
-              autoComplete="email"
-              //autoFocus
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="address"
-              label="Address"
-              name="address"
-              autoComplete="address"
-
-            />
-
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="nic"
-              label="NIC Number"
-              name="nic"
-              autoComplete="NIC"
-
-            />
-
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="contact"
-              label="Contact Number"
-              name="contact"
-              autoComplete="contact"
-            />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="contact"
+                label="Contact Number"
+                name="contact"
+                autoComplete="contact"
+                value={this.state.contact}
+                onChange={this.contacthandler}
+              />
 
 
 
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                value={this.state.password}
+                onChange={this.passwordhandler}
+              />
 
-            />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="confirmpassword"
+                label="Confirm Password"
+                type="confirmpassword"
+                id="confirmpassword"
+                value={this.state.confirmpassword}
+                onChange={this.confirmpasswordhandler}
+              />
 
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="confirmpassword"
-              label="Confirm Password"
-              type="confirmpassword"
-              id="confirmpassword"
-            />
-
-
-            <div>
-              <Button className={clsx(classes.button)}
+              <Grid item>
+              </Grid>
+              {/* <Link href="/sidebardup" variant="body2" underline="none"> */}
+              <Button onClick={this.handleSubmit} style={{
+                background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+                borderRadius: 8,
+                border: 0,
+                color: 'white',
+                height: 48,
+                width: "50%",
+                padding: '10px 30px',
+                boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+                marginBottom: '10px',
+              }}
                 type="submit"
                 variant="contained">
                 {'Sign Up'}
               </Button>
-            </div>
-            <br></br>
-          </form>
-        </Paper>
+              {/* </Link> */}
+              <br></br>
+              <Grid item >
+                Do you have an account?
+                    <Link href="/signup" variant="body2">
+                  {"Sign In"}
+                </Link>
+              </Grid>
+            </form>
+          </Paper>
+        </Grid>
       </Grid>
-    </Grid>
-  );
+    );
+  }
 }
+export default SignUp;
+
