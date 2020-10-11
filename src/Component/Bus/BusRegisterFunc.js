3b0b601b99ac2c9d247fa3131ecec09e5015bbb5
tharushiSamara <tharushisamara157@gmail.com>
2020-10-11 22:13:45 +0530

    Bus registration with frontEnd validation

--- src/Component/Bus/BusRegisterFunc.js
+++ src/Component/Bus/BusRegisterFunc.js
@@ -1,8 +1,13 @@
 import React, {useState, useEffect} from 'react'
 import { Grid, } from '@material-ui/core'
-import TextField from '@material-ui/core/TextField';
+//import Input from '@material-ui/core/TextField';
+import Input from './../Bus_Form/Input';
+import Button from '@material-ui/core/Button';
+import { Link } from "react-router-dom";
 import {makeStyles} from '@material-ui/core/styles';
 import { useForm, Form } from './../Bus_Form/useForm';
+import Checkbox from './../Bus_Form/Checkbox';
+import { FormControl, FormLabel, FormControlLabel,Checkbox as MuiCheckbox } from '@material-ui/core';
 
 
 const initialFValues = {
@@ -23,97 +28,153 @@ const initialFValues = {
 
 export default function BusRegisterFunc() {
 
+    const validate = (fieldValues = values) => {
+      let temp = {}
+      temp.busNo = values.busNo ? "" : "This field is required."
+      temp.routeNo = values.routeNo ? "" : "This field is required."
+      temp.startPoint = values.startPoint ? "" : "This field is required."
+      temp.endPoint = values.endPoint ? "" : "This field is required."
+      temp.busCapacity = values.busCapacity.length < 2 ? "" : "This field is required."
+      setErrors({
+        ...temp
+      })
+      return Object.values(temp).every(x => x=="")
+    }
+
+    const handleSubmit = e => {
+      e.preventDefault()
+      if (validate())
+        window.alert('testing..')
+    }
+
     const {
         values,
         setValues,
-        handleInputChange
-    } = useForm(initialFValues);
+        errors,
+        setErrors,
+        handleInputChange,
+        resetForm
+    } = useForm(initialFValues, true,validate);
     
     return (
-            <Form>
+            <Form onSubmit={handleSubmit}>
             <Grid container spacing={1}>
                 <Grid item xs={6}>
-                    <TextField
+                    <Input
                       variant="outlined"
                       label="Bus Number"
                       name="busNo"
                       value={values.busNo}
                       onChange={handleInputChange}
+                      error = {errors.busNo}
                     />
-                    <TextField
+                    <Input
                       variant="outlined"
                       label="Route Number"
                       name="routeNo"
                       value={values.routeNo}
                       onChange={handleInputChange}
+                      error = {errors.routeNo} 
+                      
                     />
-                    <TextField
+                    <Input
                       variant="outlined"
                       label="Start Point"
                       name="startPoint"
                       value={values.startPoint}
                       onChange={handleInputChange}
+                      error = {errors.startPoint}
+                      
                     />
-                    <TextField
+                    <Input
                       variant="outlined"
                       label="End Point"
                       name="endPoint"
                       value={values.endPoint}
                       onChange={handleInputChange}
+                      error = {errors.endPoint}
+                      
                     />
-                    <TextField
+                    <Input
                       variant="outlined"
                       label="Bus Capacity"
                       name="busCapacity"
                       value={values.busCapacity}
                       onChange={handleInputChange}
+                      error = {errors.busCapacity}
+                      
                     />
-                    <TextField           // CheckBox
-                      variant="outlined"   
-                      label="Bus Capacity"
-                      name="isReserveEnable"
-                      value={values.isReserveEnable}
-                      onChange={handleInputChange}
-                    />
+                    {/* <Checkbox
+                        name="isReserveEnable"
+                        label="Reservation Enable"
+                        lablePlacement = "start"
+                        value={values.isReserveEnable}
+                        onChange={handleInputChange}
+                    /> */}
+                    <FormControl>
+                      <FormLabel>Maximum number of reservations 20</FormLabel>
+                        <FormControlLabel
+                            control={<Checkbox color="primary" />}
+                            name="isReserveEnable"
+                            label="Reservation Enable"
+                            labelPlacement="start"
+                            value={values.isReserveEnable}
+                            onChange={handleInputChange}
+                            error = {errors.isReserveEnable}
+                            
+                          />
+                    </FormControl>
                     
                 </Grid>
                 
-                  {/* <FormControl>
-                    <FormLabel>Maximum number of reservations</FormLabel>
-                    
-                    <FormHelperText></FormHelperText>
-                  </FormControl> */}
                 <div>
-                    <TextField
+                    <Input
                         variant="outlined"
                         label="Start Point"
                         name="forwardStartPoint"
                         value={values.forwardStartPoint}
                         onChange={handleInputChange}
+                        error = {errors.forwardStartPoint}
+                        
                     />
-                    <TextField
+                    <Input
                         variant="outlined"
                         label="Departure Time"
                         name="forwardDepartTime"
                         value={values.forwardDepartTime}
                         onChange={handleInputChange}
+                        error = {errors.forwardDepartTime}
+                        
                     />
                 </div>
                 <div>
-                    <TextField
+                    <Input
                         variant="outlined"
                         label="Start Point"
                         name="backwardStartPoint"
                         value={values.backwardStartPoint}
                         onChange={handleInputChange}
+                        error 
                     />
-                    <TextField
+                    <Input
                         variant="outlined"
                         label="Departure Time"
                         name="backwardDepartTime"
                         value={values.backwardDepartTime}
                         onChange={handleInputChange}
+                        error
+                        
                     />
+                  
+                   <div>
+                      <Button className="button" color="primary" 
+                          type="submit"
+                          variant="contained">
+                          {'Register'}
+                          <Link to="/viewBuses"></Link>
+                      </Button>
+                  </div>
+                    
 
                 </div>
               
